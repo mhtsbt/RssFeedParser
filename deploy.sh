4e98c4090d1d173156e85e8addb9657ed82368e7
Matthias Hutsebaut <matthias@MatthiasBook.local>
2017-11-04 22:42:24 +0100

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 Source=$2
 
-dotnet pack ./RssFeedParser/RssFeedParser.csproj
+dotnet pack ./RssFeedParser/RssFeedParser.csproj -c Release
 dotnet nuget push ./RssFeedParser/RssFeedParser.nupkg -k $ApiKey -s $Source
